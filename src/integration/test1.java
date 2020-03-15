d1943a8cffbdb59ed18f35268fafc4926377655d
meghana12pu <flyhigh.meghana@gmail.com>
2020-03-15 12:10:13 +0530

    first commit

---
+++ src/integration/test1.java
@@ -0,0 +1,12 @@
+package integration;
+
+
+
+import org.testng.annotations.Test;
+
+public class test1 {
+@Test
+public void m2(){
+System.out.println("second test");
+}
+}
---
+++ src/integration/test2.java
@@ -0,0 +1,10 @@
+package integration;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+@Test
+public void m1(){
+System.out.println("second test");
+}
+}
